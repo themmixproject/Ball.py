16b754b1a9cd1ad20096fb606e66dc257b412505
Steven de Valk <s1129787@student.windesheim.nl>
2019-11-09 16:20:06 +0100

    basic ball bounce animation

--- ball.py
+++ ball.py
@@ -1,3 +1,9 @@
+# if (ball["y"] + (ball["width"]/2) + verticalSpeed) > 500:
+#    verticalSpeed = -verticalSpeed * friction
+# else:
+#    verticalSpeed += gravity
+
+# print(verticalSpeed)
 
 # imports
 import pygame
@@ -14,10 +20,13 @@ winWidth = 500
 
 black = (0, 0, 0)
 white = (255, 255, 255)
+red = (255, 0, 0)
 
 
 # setup window
 window = pygame.display.set_mode([winWidth, winHeight])
+
+window.fill(black)
 
 # get center
 center = {
@@ -28,20 +37,64 @@ center = {
 center["x"], center["y"] = map( lambda x : int(x/2), pygame.display.get_surface().get_size() )
 
 # ball coordinates
+
+# TO MORE AWAKE STEVEN IMPROVE THIS
 ball = {
     "x": center["x"],
     "y": center["y"],
-    "radius": 30
+    "radius": 30,
+    "img": pygame.image.load("circ.png"),
+    "size": pygame.image.load("circ.png").get_size(),
+    "width": pygame.image.load("circ.png").get_size()[0],
+    "height": pygame.image.load("circ.png").get_size()[1]
 }
 
-clock = pygame.time.Clock()
+print(ball)
 
-frameTime = 0
+gravity = 0.001
 
-window.fill(black)
+friction = 0.89
 
-def updateBall(dt):
-    drawCirc(ball["x"], ball["y"], ball["radius"], white)
+verticalSpeed = 0.01
+
+def updateBall(dt=1):
+    global verticalSpeed, friction, gravity
+
+
+    # "deletes" old ball
+    pygame.draw.rect(window, black, (ball["x"], ball["y"], ball["img"].get_size()[0], ball["img"].get_size()[1]))
+    
+
+
+    # if (ball["y"] + (ball["width"]/2) + verticalSpeed) > 500:
+    #    verticalSpeed = -verticalSpeed * friction
+    # else:
+    #    verticalSpeed += gravity
+
+    if ball["y"] + ball["height"] + verticalSpeed > 500:
+        verticalSpeed = -verticalSpeed * friction
+    else:
+        verticalSpeed += gravity
+
+    ball["y"] += 1 * verticalSpeed * dt
+
+    # if ball["y"] + ball["height"] > 500:
+    #     ball["y"] = 250
+    # else:
+    #     ball["y"] += 1 * dt
+
+
+    temp = pygame.Surface(ball["img"].get_rect().size, pygame.SRCALPHA)
+    temp.blit(ball["img"], (0, 0))
+    temp.convert_alpha()
+
+    # window.blit( ball["img"], (ball["x"], ball["y"]) )
+    window.blit(temp,( ball["x"], ball["y"]))
+
+    # posx += dirx * speed * deltatime
+    
+    
+    
 
 
 def eventHandler(event):
@@ -49,15 +102,20 @@ def eventHandler(event):
         print("quit " + str(event))
         pygame.quit()
         quit()
+    mouseButton = pygame.mouse.get_pressed()
+    if mouseButton[0] == 1:
+        pygame.draw.rect(window, black, (ball["x"], ball["y"], ball["img"].get_size()[0], ball["img"].get_size()[1]))
+        ball["y"] = 250
+
 
 def core():
+    clock = pygame.time.Clock()
     dt = clock.get_time()
 
     while True:
 	#Input check 
-        # make this more efficient
-        # AS IN, MAYBE JUST INCLUDE THE CLICK EVENTS
 
+        # AS IN, MAYBE JUST INCLUDE THE CLICK EVENTS
         for event in pygame.event.get():
             eventHandler(event)
             # print(str(event))
@@ -66,7 +124,7 @@ def core():
 
 	#Draw stuff
 
-        print(dt)
+        # print(dt)
 
         pygame.display.update()
         dt = clock.tick()
